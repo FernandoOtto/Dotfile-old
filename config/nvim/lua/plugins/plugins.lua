96ed3b17cffc3899ef6120a385d2ec1872bc7b6c
Fernando Otto <126904289+FernandoOtto@users.noreply.github.com>
2023-09-16 12:12:06 -0700

    Add files via upload

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -1,1 +1,12 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'terroo/vim-simple-emoji'
+  use 'elvessousa/sobrio'
+  use 'windwp/nvim-autopairs'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+end)
 
