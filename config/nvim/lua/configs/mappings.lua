6311b8b01719bdce8b74f6bfd737cdad390c766e
Fernando Otto <126904289+FernandoOtto@users.noreply.github.com>
2023-09-16 12:11:08 -0700

    Add files via upload

--- config/nvim/lua/configs/mappings.lua
+++ config/nvim/lua/configs/mappings.lua
@@ -1,1 +1,8 @@
+-- Sair com 'q' , se usar gravação de macros altere para <C-q> → Ctrl + q
+vim.cmd([[ map q :q<CR> ]])
 
+-- Para Salvar com 'Ctrl + S' nos modos: Normal, Inserção e Visual
+-- Precisa adicionar a linha: stty -ixon , ao seu ~/.bashrc
+vim.cmd([[ nnoremap <C-s> :w<CR> ]])
+vim.cmd([[ inoremap <C-s> <Esc>:w<CR>l ]])
+vim.cmd([[ vnoremap <C-s> <Esc>:w<CR> ]])
